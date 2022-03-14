08f4ebe280eab5d980c6141018c76f55c3f2a519
Balint Szilagyi <s2064308@ed.ac.uk>
2022-03-14 18:16:03 +0000

    3.1

--- graph.py
+++ graph.py
@@ -1,3 +1,4 @@
+from email.mime import base
 import math
 import random
 
@@ -14,19 +15,55 @@ class Graph:
     # the n>0 case, where we read a general graph in a different format.
     # self.perm, self.dists, self.n are the key variables to be set up.
     def __init__(self,n,filename):
-        pass
-
+        with open(filename, "r") as f:
+            if n < 0:
+                lines = list(map(lambda x: list(map(int, x.split("  "))), list(map(str.strip, f.readlines()))))
+            else:
+                lines = list(map(lambda x: list(map(int, x.split(" "))), list(map(str.strip, f.readlines()))))
+        f.close()
+        if n < 0: 
+            self.n = len(lines)
+            self.dists = [[euclid(lines[i], lines[j]) for j in range(self.n)] for i in range(self.n)]
+        else: 
+            self.n = n
+            self.dists = [[0 for j in range(self.n)] for i in range(self.n)]
+            for node in lines:
+                start = node[0]
+                end = node[1]
+                weight = node[2]
+                self.dists[start][end] = weight
+                self.dists[end][start] = weight
+        self.perm = [i for i in range(self.n)]
+        self.tour = float("inf")
 
     # Complete as described in the spec, to calculate the cost of the
     # current tour (as represented by self.perm).
     def tourValue(self):
-        pass
+        cost = 0
+        current = self.perm[0]
+        for next in self.perm[1:]:
+            cost += self.dists[current][next] # potential bad
+            #print(current,"-",self.dists[current][next],"->",next, ":", cost)
+            current = next
+        cost += self.dists[self.perm[0]][current]
+        #print(self.perm[0],"-",self.dists[self.perm[0]][current],"->",next, ":", cost)
+        self.tour = cost
 
     # Attempt the swap of cities i and i+1 in self.perm and commit
     # commit to the swap if it improves the cost of the tour.
     # Return True/False depending on success.
     def trySwap(self,i):
-        pass
+        basecost = self.tour
+        if i+1 >= self.n:
+            return False
+        self.perm[(i+1)], self.perm[i] = self.perm[i], self.perm[(i+1)]
+        self.tourValue()
+        if self.tour < basecost: 
+            return True
+        else: 
+            self.perm[(i+1)], self.perm[i] = self.perm[i], self.perm[(i+1)]
+            self.tour = basecost
+            return False
 
 
     # Consider the effect of reversiing the segment between
--- tests.py
+++ tests.py
@@ -2,4 +2,9 @@ import math
 import graph
 
 
-
+g=graph.Graph(12,"twelvenodes")
+g.tourValue()
+print(g.tour, g.perm)
+g.swapHeuristic(12)
+g.tourValue()
+print(g.tour, g.perm)
